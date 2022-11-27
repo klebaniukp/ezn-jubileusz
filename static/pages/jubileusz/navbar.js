bcac2545e6041147a615fabc6775280de8ec4070
klebaniukp <klebaniukp@gmail.com>
2022-11-27 23:06:48 +0100

    navbar

---
+++ static/pages/jubileusz/navbar.js
@@ -0,0 +1,37 @@
+const input = document.querySelector('#burger-activator');
+const navbar = document.querySelector('.navbar')
+
+const listItems = document.querySelectorAll(".menu-mobile-item");
+const mobileMenu = document.querySelector(".menu-mobile");
+
+function manageMobileMenu() {
+    changeBackgroundColor();
+    manageMenuMovement();
+}
+
+function manageMenuMovement() {
+    if (input.checked) {
+        mobileMenu.classList.add("menu-mobile-expand");
+    }
+    if(!input.checked) {
+        mobileMenu.classList.remove("menu-mobile-expand");
+    }
+}
+
+function changeBackgroundColor() {
+    if (input.checked) {
+        navbar.classList.add("navbar-light");
+    } else {
+        navbar.classList.remove("navbar-light");
+    }
+}
+
+function closeMenu() {
+    input.click();
+}
+
+input.addEventListener("click", manageMobileMenu);
+
+listItems.forEach((item) => {
+    item.addEventListener("click", closeMenu);
+})
